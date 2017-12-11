6538f0b5c102191f517ca5ee0a42d764b4160991
Alvin Thai <alvtdev@gmail.com>
2017-12-10 19:45:10 -0800

    renamed main.py, added sample test and training data

--- getPlaylists.py
+++ getPlaylists.py
@@ -26,6 +26,8 @@ else:
 cache = '.tokencache'
 
 def getArtistGenres(uri, sp):
+    #print "Artist uri: ",
+    #print uri
     artist = sp.artist(uri)
     return artist['genres']
 
@@ -35,13 +37,17 @@ def getAlbumGenres(uri, sp):
 
 def printTrackDetails(sp, tracks):
     i = 1
-    
+    #dataHeader = "Song Name \t Artist Name \t Genres \t "
+    #dataHeader += "Acousticness \t Danceability \t Energy \t"
+    #dataHeader += "Instrumentalness \t Liveness \t Speechiness \n"
+    #F.write(dataHeader)
     while True:
         for item in tracks['items']:
             track = item['track']
             print track['name'] + ' - ' + track['artists'][0]['name'] + ' - ',
             print track['album']['name'] + ' - ',
-            genres = getArtistGenres(track['artists'][0]['uri'], sp)
+            artistURI = track['artists'][0]['uri'];
+            genres = getArtistGenres(artistURI, sp)
             genreList = [genre.encode('UTF8') for genre in genres]
             print genreList
             #song_title = str(i) + '. ' + track['name'] +' - '
@@ -57,39 +63,43 @@ def printTrackDetails(sp, tracks):
 #function to generate a comma delimited file with data
 def printTracksToFile(sp, tracks, F):
     i = 1
-    dataHeader = "Song Name \t Artist Name \t Genres \t "
-    dataHeader += "Acousticness \t Danceability \t Energy \t"
-    dataHeader += "Instrumentalness \t Liveness \t Speechiness \n"
-    F.write(dataHeader)
-    #pp = pprint.PrettyPrinter(depth=6)
+    pp = pprint.PrettyPrinter(depth=6)
     while True:
         for item in tracks['items']:
             track = item['track']
             uri = [track['uri'].split(":")[2]]
             trackinfo = sp.audio_features(uri)
 
-            #append track metadata to string, tab separated
-            trackdata = track['name'] + "\t" + track['artists'][0]['name'] + "\t"
-            #trackdata += track['album']['name'] + '\t'
-            genres = getArtistGenres(track['artists'][0]['uri'], sp)
-            genreList = [genre.encode('UTF8') for genre in genres]
-            if len(genreList) > 0:
-                for genre in genres:
-                    trackdata += genre + "," 
-                trackdata = trackdata[:-1] +"\t"
-            else:
-                trackdata += "none \t"
+            artistURI = track['artists'][0]['uri'];
+            #if artist exists
+            if artistURI:
+                #append track metadata to string, tab separated
+                trackdata = track['name'].encode('utf-8') + "\t"
+                trackdata += track['artists'][0]['name'].encode('utf-8') + "\t"
+                genres = getArtistGenres(artistURI, sp)
+                genreList = [genre.encode('utf-8') for genre in genres]
+                if len(genreList) > 0:
+                    for genre in genres:
+                        trackdata += genre.encode('utf-8')+ "," 
+                    trackdata = trackdata[:-1] +"\t"
+                else:
+                    trackdata += "none \t"
+                #print trackdata + "\n"
 
-            #append other characteristics from audio_features()
-            trackdata += str(trackinfo[0]['acousticness']) + "\t" 
-            trackdata += str(trackinfo[0]['danceability']) + "\t" 
-            trackdata += str(trackinfo[0]['energy']) + "\t" 
-            trackdata += str(trackinfo[0]['instrumentalness']) + "\t" 
-            trackdata += str(trackinfo[0]['liveness']) + "\t" 
-            trackdata += str(trackinfo[0]['speechiness']) + "\t" 
-        
-            #print trackdata
-            F.write(trackdata + "\n")
+                #append other characteristics from audio_features()
+                trackdata += str(trackinfo[0]['acousticness']) + "\t" 
+                trackdata += str(trackinfo[0]['danceability']) + "\t" 
+                trackdata += str(trackinfo[0]['energy']) + "\t" 
+                trackdata += str(trackinfo[0]['instrumentalness']) + "\t" 
+                trackdata += str(trackinfo[0]['liveness']) + "\t" 
+                trackdata += str(trackinfo[0]['speechiness']) + "\t" 
+            
+                #print trackdata
+                F.write(trackdata + "\n")
+            #else:
+                #print "ERROR: ARTIST URI NOT FOUND"
+                #pp.pprint(track)
+                #sys.exit()
             i += 1
         #check if there are more pages
         if tracks['next']:
@@ -108,6 +118,10 @@ def getPlaylists(sp, username):
     playlists = sp.user_playlists(username)
     filename = "playlists-" + username + ".tsv"
     F = open(filename, "w")
+    dataHeader = "Song Name \t Artist Name \t Genres \t "
+    dataHeader += "Acousticness \t Danceability \t Energy \t"
+    dataHeader += "Instrumentalness \t Liveness \t Speechiness \n"
+    F.write(dataHeader)
     while True:
         for playlist in playlists['items']:
             if playlist['name'] is not None:
